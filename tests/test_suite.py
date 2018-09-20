772d34cb6f934ae216f0fa0d34c26999469fea01
Julian <jules89@arcor.de>
2018-09-20 15:40:53 -0400

    make test suit fail

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -13,13 +13,6 @@ testmodules = [
 suite = unittest.TestSuite()
 
 for t in testmodules:
-    try:
-        # If the module defines a suite() function, call it to get the suite.
-        mod = __import__(t, globals(), locals(), ['suite'])
-        suitefn = getattr(mod, 'suite')
-        suite.addTest(suitefn())
-    except (ImportError, AttributeError):
-        # else, just load all the tests cases from the module.
-        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
 
 unittest.TextTestRunner().run(suite)
