b4201e9d4b2b5254adce385b3d1688a3605da923
Julian <jules89@arcor.de>
2018-09-20 15:38:57 -0400

    Fix test method for gitlab

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,4 +1,9 @@
+import os
+import sys
 import unittest
+
+# add the path to be execute in the main directory
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 
 testmodules = [
     'tests.test_correctness',
