b8209e89b4c1e2a606df99dc6eb63909c3d76327
proteekroy <royprote@msu.edu>
2019-01-30 00:50:16 -0500

    CTLZ problems

--- pymop/problems/cdtlz.py
+++ pymop/problems/cdtlz.py
@@ -1,18 +1,20 @@
 import autograd.numpy as anp
-
-from pymop.problems.dtlz import DTLZ1, DTLZ2, DTLZ3, DTLZ4
-
+import matplotlib.pyplot as plt
+from pymop.problems.dtlz import DTLZ, DTLZ1, DTLZ2, DTLZ3, DTLZ4
+from pymop.util import UniformReferenceDirectionFactory
 
 class C1DTLZ1(DTLZ1):
 
-    def __init__(self, n_var=7, n_obj=3, **kwargs):
+    def __init__(self, n_var=12, n_obj=3, **kwargs):
         super().__init__(n_var, n_obj, **kwargs)
         self.n_constr = 1
 
     def _evaluate(self, X, out, *args, **kwargs):
         super()._evaluate(X, out, *args, **kwargs)
-        F = out["F"]
-        out["G"] = - (1 - F[:, -1] / 0.6 - anp.sum(F[:, :-1] / 0.5, axis=1))
+        out["G"] = constraint_c1_linear(out["F"])
+
+    def _calc_pareto_front(self, ref_dirs, *args, **kwargs):
+        return super()._calc_pareto_front(ref_dirs, *args, **kwargs)
 
 
 class C1DTLZ3(DTLZ3):
@@ -33,8 +35,10 @@ class C1DTLZ3(DTLZ3):
 
     def _evaluate(self, X, out, *args, **kwargs):
         super()._evaluate(X, out, *args, **kwargs)
-        _r = anp.sum(out["F"] ** 2, axis=1)
-        out["G"] = - (_r - 4 ** 2) * (_r - self.r ** 2)
+        out["G"] = constraint_c1_spherical(out["F"], self.r)
+
+    def _calc_pareto_front(self, ref_dirs, *args, **kwargs):
+        return super()._calc_pareto_front(ref_dirs, *args, **kwargs)
 
 
 class C2DTLZ2(DTLZ2):
@@ -58,12 +62,9 @@ class C2DTLZ2(DTLZ2):
         out["G"] = constraint_c2(out["F"], self.r)
 
     def _calc_pareto_front(self, ref_dirs, *args, **kwargs):
-
-        F = super().pareto_front(ref_dirs, *args, **kwargs)
+        F = super()._calc_pareto_front(ref_dirs, *args, **kwargs)
         G = constraint_c2(F, r=self.r)
         G[G <= 0] = 0
-        if G.ndim > 1:
-            G = anp.sum(G, axis=1)
         return F[G <= 0]
 
 
@@ -71,14 +72,14 @@ class C3DTLZ4(DTLZ4):
 
     def __init__(self, n_var=7, n_obj=3, **kwargs):
         super().__init__(n_var, n_obj, **kwargs)
-        self.n_constr = 1
+        self.n_constr = n_obj
 
     def _evaluate(self, X, out, *args, **kwargs):
         super()._evaluate(X, out, *args, **kwargs)
-        out["G"] = constraint_c3(out["F"], 4)
+        out["G"] = constraint_c3_spherical(out["F"])
 
     def _calc_pareto_front(self, ref_dirs, *args, **kwargs):
-        F = super().pareto_front(ref_dirs, *args, **kwargs)
+        F = super()._calc_pareto_front(ref_dirs, *args, **kwargs)
         a = anp.sqrt(anp.sum(F ** 2, 1) - 3 / 4 * anp.max(F ** 2, axis=1))
         a = anp.expand_dims(a, axis=1)
         a = anp.tile(a, [1, ref_dirs.shape[1]])
@@ -87,13 +88,16 @@ class C3DTLZ4(DTLZ4):
         return F
 
 
-def constraint_c1(f, r, dtlz_type):
-    if dtlz_type == 3:
-        radius = anp.sum(f ** 2, axis=1)
-        g = - (radius - 16) * (radius - r ** 2)
+def constraint_c1_linear(f):
 
-    elif dtlz_type == 1:
-        g = - (1 - f[:, -1] / 0.6 - anp.sum(f[:, :-1] / 0.5, axis=1))
+    g = - (1 - f[:, -1] / 0.6 - anp.sum(f[:, :-1] / 0.5, axis=1))
+
+    return g
+
+
+def constraint_c1_spherical(f, r):
+    radius = anp.sum(f ** 2, axis=1)
+    g = - (radius - 16) * (radius - r ** 2)
 
     return g
 
@@ -114,24 +118,169 @@ def constraint_c2(f, r):
     return g
 
 
-def constraint_c3(f, dtlz_type):  # M circle if DTLZ2,3,4 and linear otherwise
+def constraint_c3_linear(f):  # M lines
     n_obj = f.shape[1]
-    g = []  # anp.zeros(f.shape)
+    g = []
 
     for i in range(n_obj):
-        if dtlz_type == 1:
-            _g = 1 - f[:, i] / 0.5 - (anp.sum(f, axis=1) - f[:, i])
-        elif dtlz_type == 2 or dtlz_type == 3 or dtlz_type == 4:
-            _g = 1 - f[:, i] ** 2 / 4 - (anp.sum(f ** 2, axis=1) - f[:, i] ** 2)
-        else:
-            raise Exception("DTLZ type not supported for C3 constrained problem")
+        _g = 1 - f[:, i] / 0.5 - (anp.sum(f, axis=1) - f[:, i])
+        g.append(_g)
+
+    return anp.column_stack(g)
+
+
+def constraint_c3_spherical(f):  # M ellipse
+    n_obj = f.shape[1]
+    g = []
+
+    for i in range(n_obj):
+        _g = 1 - f[:, i] ** 2 / 4 - (anp.sum(f ** 2, axis=1) - f[:, i] ** 2)
         g.append(_g)
     return anp.column_stack(g)
 
 
-def constraint_c4(f, r):  # cylindrical
+def constraint_c4_cylindrical(f, r):  # cylindrical
     l = anp.mean(f, axis=1)
     l = anp.expand_dims(l, axis=1)
     g = -anp.sum(anp.power(f - l, 2), axis=1) + anp.power(r, 2)
 
     return g
+
+
+class CDTLZ(DTLZ):
+
+    def __init__(self, dtlz, clist=None, rlist=None, **kwargs):
+
+        assert len(clist) == len(rlist)
+        self.rlist = anp.asarray(rlist)
+        self.clist = anp.asarray(clist)
+        self.dtlz = dtlz
+
+        super().__init__(n_var=dtlz.n_var, n_obj=dtlz.n_obj, **kwargs)
+        self.n_constr = len(anp.unique(clist))
+
+        if isinstance(dtlz, DTLZ1):
+            self.dtlz_type = 1
+        elif isinstance(dtlz, DTLZ2):
+            self.dtlz_type = 2
+        elif isinstance(dtlz, DTLZ3):
+            self.dtlz_type = 3
+        elif isinstance(dtlz, DTLZ4):
+            self.dtlz_type = 4
+        else:
+            raise Exception("DTLZ problem not supported.")
+
+    def _evaluate(self, X, out, *args, **kwargs):
+
+        # merge the result we get from the dtlz
+        self.dtlz._evaluate(X, out, *args, **kwargs)
+
+        g = []
+        for i in range(self.n_constr):
+            if self.clist[i] == 1:
+                if self.dtlz_type == 1:
+                    _g = constraint_c1_linear(out["F"])
+                else:
+                    _g = constraint_c1_spherical(out["F"], self.rlist[i])
+            elif self.clist[i] == 2:
+                _g = constraint_c2(out["F"], self.rlist[i])
+            elif self.clist[i] == 3:
+                if self.dtlz_type == 1:
+                    _g = constraint_c3_linear(out["F"])
+                else:
+                    _g = constraint_c3_spherical(out["F"])
+            elif self.self.clist[i] == 4:
+                _g = constraint_c4_cylindrical(out["F"], self.rlist[i])
+            else:
+                _g = []
+
+            g.append(_g)
+
+        out["G"] = anp.column_stack(g)
+
+    def _calc_pareto_front(self, ref_dirs, *args, **kwargs):
+
+        F = self.dtlz.pareto_front(ref_dirs, *args, **kwargs)
+
+        if anp.any(self.clist == 3):
+            a = anp.sqrt(anp.sum(F ** 2, 1) - 3 / 4 * anp.max(F ** 2, axis=1))
+            a = anp.expand_dims(a, axis=1)
+            a = anp.tile(a, [1, ref_dirs.shape[1]])
+            F = F / a
+        elif anp.any(self.clist == 2):
+            r = self.rlist[anp.where(self.clist == 2)][0]
+            _g = constraint_c2(F, r)
+            _g[_g <= 0] = 0
+            F = F[_g <= 0]
+        elif anp.any(self.clist == 4):
+            r = self.rlist[anp.where(self.clist == 4)][0]
+            _g = constraint_c4_cylindrical(F, r)
+            _g[_g <= 0] = 0
+            F = F[_g <= 0]
+
+        return F
+
+
+problems = [('CDTLZ', [DTLZ3(n_var=2, n_obj=2), [1, 3], [6, 0]])]
+
+if __name__ == '__main__':
+
+    for prob_no, entry in enumerate(problems):
+        name, params = entry
+        print("Plotting: " + name)
+        problem = globals()[name](*params)
+        if isinstance(problem.dtlz, DTLZ1):
+            dtlz_type = 1
+        elif isinstance(problem.dtlz, DTLZ2):
+            dtlz_type = 2
+        elif isinstance(problem.dtlz, DTLZ3):
+            dtlz_type = 3
+        elif isinstance(problem.dtlz, DTLZ4):
+            dtlz_type = 4
+        else:
+            raise Exception("DTLZ problem not supported.")
+
+        nx = 100
+
+        x = anp.linspace(0, 1, nx)
+
+        if dtlz_type == 1:
+            # y = np.linspace(0.5 - 7.5 * 1e-2, 0.5 + 7.5 * 1e-2, nx)  # DTLZ1
+            y = anp.linspace(0.5 - 7.5 * 1e-4, 0.5 + 7.5 * 1e-4, nx)  # DTLZ1
+        elif dtlz_type == 2:
+            y = anp.linspace(0.5 - 0.2, 0.5 + 0.2, nx)
+        elif dtlz_type == 3:
+            y = anp.linspace(0.5 - 7.5 * 1e-3, 0.5 + 7.5 * 1e-3, nx)  # DTLZ3
+        elif dtlz_type == 4:
+            y = anp.linspace(0.5 - 0.4, 0.5 + 0.4, nx)
+
+        xv, yv = anp.meshgrid(x, y)
+        x = anp.vstack(xv.flatten())
+        y = anp.vstack(yv.flatten())
+        X = anp.hstack((x, y))
+        n = x.shape[0]
+
+        out = dict()
+        G = anp.zeros((n, problem.n_constr))
+        problem._evaluate(X, out)
+        F = out["F"]
+        G = out["G"]
+
+        F1 = F[:, 0].flatten()
+        F2 = F[:, 1].flatten()
+
+        G[G <= 0] = 0
+        if G.ndim > 1:
+            G = anp.sum(G, axis=1)
+        index = G > 0
+
+        ref_dirs = UniformReferenceDirectionFactory(n_dim=problem.n_obj, n_points=21).do()
+        PF = problem._calc_pareto_front(ref_dirs)
+
+        plt.title(name)
+        plt.plot(F1, F2, 'o', color='#DCDCDC')  # '#A9A9A9'
+        plt.plot(F1[index], F2[index], 'o', color='b')
+        plt.plot(PF[:, 0], PF[:, 1], 'o', color='r')
+
+    plt.show()
+
